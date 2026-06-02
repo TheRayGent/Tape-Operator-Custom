ef319931e2103a1d169482c0050a5c302ffc52f0
Kirills Reunovs <Kirlovon@outlook.com>
2026-06-02 18:27:39 +0200

    Update required script version to 3.3.3

--- player/config.js
+++ player/config.js
@@ -1,5 +1,5 @@
 // Latest required script version
-const REQUIRED_VERSION = '3.3.1';
+const REQUIRED_VERSION = '3.3.3';
 
 // URL to Kinobox API
 const KINOBOX_API = 'https://api.kinobox.tv/api/players';
